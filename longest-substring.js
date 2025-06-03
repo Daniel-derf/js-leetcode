0d5a27869681c886aa5cf6294871b2ad7fdb7636
Daniel-derf <danielpardinho321@gmail.com>
2025-06-03 16:23:38 -0300

    new longest substring implementation

--- longest-substring.js
+++ longest-substring.js
@@ -2,31 +2,34 @@
 Given a string, return its longest substring without repeating characters
 */
 
-function longestSubstring(string) {
-  let [left, right] = [0, 0];
-  let maxLength = 0;
+function longestSubstring(s) {
+  let [l, r] = [0, 0];
 
-  const set = new Set();
+  const map = {};
+  let maxLen = 0;
 
-  while (right < string.length) {
-    if (set.has(string[right])) {
-      while (set.has(string[right])) {
-        set.delete(string[left]);
-        left += 1;
-      }
+  while (r < s.length) {
+    const char = s[r];
+
+    while (map[char] && map[char] > 0) {
+      const removedChar = s[l];
+
+      map[removedChar]--;
+      l++;
     }
 
-    set.add(string[right]);
-    const dif = right - left + 1;
-    maxLength = Math.max(maxLength, dif);
+    map[char] = 1;
 
-    right += 1;
+    const curLen = r - l + 1;
+    maxLen = Math.max(maxLen, curLen);
+
+    r++;
   }
 
-  return maxLength;
+  return maxLen;
 }
 
-const input = "abcarol";
+const input = "pwwkew";
 
 const output = longestSubstring(input);
 
