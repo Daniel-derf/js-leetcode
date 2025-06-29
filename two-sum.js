0b7641115b1085ba1ebe480b48da58c3323d5bfa
Daniel-derf <danielpardinho321@gmail.com>
2025-06-29 18:51:57 -0300

    two sum

--- two-sum.js
+++ two-sum.js
@@ -1,19 +1,19 @@
 var twoSum = function (nums, target) {
-  const map = {};
+  const hash = {};
 
   for (let i = 0; i < nums.length; i++) {
-    const curVal = nums[i];
+    const num = nums[i];
 
-    const remaining = target - curVal;
+    const remaining = target - num;
 
-    if (map[remaining] != undefined) return [map[remaining], i];
+    if (hash[remaining] !== undefined) return [hash[remaining], i];
 
-    map[curVal] = i;
+    hash[num] = i;
   }
 };
 
-const nums = [3, 2, 4];
-const target = 6;
+const nums = [2, 7, 11, 15];
+const target = 9;
 
 const output = twoSum(nums, target);
 
