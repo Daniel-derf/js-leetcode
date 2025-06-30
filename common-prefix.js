0daa220a0dc69301c29aa873017f142f7a96849a
Daniel-derf <danielpardinho321@gmail.com>
2025-06-30 18:17:34 -0300

    roman to integer num

--- common-prefix.js
+++ common-prefix.js
@@ -3,21 +3,27 @@
  * @return {string}
  */
 var longestCommonPrefix = function (strs) {
-  let maxPrefix = strs[0];
+  strs.sort();
 
-  for (const str of strs) {
-    for (let i = 0; i <= str.length; i++) {
-      if (maxPrefix[i] !== str[i]) {
-        if (i === 0) return "";
-        maxPrefix = maxPrefix.substring(0, i);
-        break;
-      }
+  const firstW = strs[0];
+  const lastW = strs[strs.length - 1];
+
+  let max = firstW;
+
+  const len = Math.max(max.length, lastW.length);
+
+  for (let i = 0; i < len; i++) {
+    if (max[i] !== lastW[i]) {
+      if (i === 0) return "";
+
+      max = max.substring(0, i);
+      return max;
     }
   }
 
-  return maxPrefix;
+  return "";
 };
 
-let strs = ["ab", "a"];
+let strs = ["a"];
 let output = longestCommonPrefix(strs);
 console.log(output);
